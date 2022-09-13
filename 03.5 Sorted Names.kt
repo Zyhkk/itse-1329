35317d8744b9339f586db43dff26df8ac208ed23
Eric 1329-FA <111894460+Zyhkk@users.noreply.github.com>
2022-09-13 10:47:46 -0500

    Create 03.5 Sorted Names.kt

---
+++ 03.5 Sorted Names.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+fun main() {
+
+val 
+println("We're going to sort three names today") 
+
+  println("Enter the first name: ")
+  
+
+
+
+  
+val names = listOf(name1 , name2, name3)
+
+val sortedNames = numbers.sortedByDescending { it.first() }
+println("Sorted by : $sortedNames")
+}
