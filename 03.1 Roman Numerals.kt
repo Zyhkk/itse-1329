eb41e51e38602bcbeec0e1db31748f3e6f38b27a
Eric 1329-FA <111894460+Zyhkk@users.noreply.github.com>
2022-09-13 09:12:27 -0500

    Create 03.1 Roman Numerals.kt

---
+++ 03.1 Roman Numerals.kt
@@ -0,0 +1,20 @@
+fun main() {
+    
+    println("Enter a number between 1 and 10.")
+    var x = Integer.valueOf(readLine())
+    
+    when (x) {
+    1 -> print("I")
+    2 -> print("II")
+    3 -> print("III")
+    4 -> print("VI")
+    5 -> print("V")
+    6 -> print("IV")
+    7 -> print("VII")
+    8 -> print("VIII")
+    9 -> print("IX")
+    10 -> print("X")
+    else -> {
+        print("error")
+}
+}
