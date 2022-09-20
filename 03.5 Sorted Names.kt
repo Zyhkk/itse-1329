99e01b20d6819afd17fabe4ecb2b1d17e829ddad
Eric 1329-FA <111894460+Zyhkk@users.noreply.github.com>
2022-09-20 09:20:53 -0500

    Update 03.5 Sorted Names.kt

--- 03.5 Sorted Names.kt
+++ 03.5 Sorted Names.kt
@@ -1,17 +1,23 @@
 import java.util.Scanner
 fun main() {
 
-val 
-println("We're going to sort three names today") 
+  println("We're going to sort three names today") 
 
   println("Enter the first name: ")
+
+  val scanner  = Scanner(System.`in`)
+  val name1 = scanner.nextLine()
   
+  println("Enter the second name: ")
 
-
-
+  val name2 = scanner.nextLine()
   
-val names = listOf(name1 , name2, name3)
+  println("Enter the third name: ")
 
-val sortedNames = numbers.sortedByDescending { it.first() }
-println("Sorted by : $sortedNames")
+  val name3 = scanner.nextLine()  
+
+  val names = mutableListOf(name1,name2,name3)
+
+    names.sort()
+    println(names)
 }
