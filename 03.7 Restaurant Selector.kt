c909880847b1330fabcc184a72603d49933b9e86
Eric 1329-FA <111894460+Zyhkk@users.noreply.github.com>
2022-09-22 10:50:40 -0500

    Create 03.7 Restaurant Selector.kt

---
+++ 03.7 Restaurant Selector.kt
@@ -0,0 +1,17 @@
+fun main (){
+  println("Today w're going to decide a good place to eat for your group.")
+  println("Is anyone Vegetarian?(yes/no)")
+  val input1 = readLine()
+  println("Is anyone Vegan?(yes/no)")
+  val input2 = readLine()
+  println("Is anyone Gluten Free?(yes/no)")
+  val input3 = readLine()
+
+  println("Here are your resturant options: ")
+
+  if (input1 == yes){
+    
+    println("The Chef’s Kitchen")
+      if(input2 == yes && input)
+  }
+}
