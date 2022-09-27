d5ee7fd7775731337ea786089bd9d040575b5bb7
Eric 1329-FA <111894460+Zyhkk@users.noreply.github.com>
2022-09-27 10:47:52 -0500

    Update 03.7 Restaurant Selector.kt

--- 03.7 Restaurant Selector.kt
+++ 03.7 Restaurant Selector.kt
@@ -1,5 +1,5 @@
 fun main (){
-  println("Today w're going to decide a good place to eat for your group.")
+  println("Today we're going to decide a good place to eat for your group.")
   println("Is anyone Vegetarian?(yes/no)")
   val input1 = readLine()
   println("Is anyone Vegan?(yes/no)")
@@ -9,9 +9,39 @@ fun main (){
 
   println("Here are your resturant options: ")
 
-  if (input1 == yes){
-    
-    println("The Chef’s Kitchen")
-      if(input2 == yes && input)
-  }
+ if(input1 == "no" && input2 == "no"){
+   if(input3 == "no"){
+   println("Joe’s Gourmet Burgers")
+   println("Main Street Pizza Company")
+   println("Corner Café")
+   println("Mama’s Fine Italian")
+   println("The Chef’s Kitchen")
+ }
+ }else if(input1 == "yes" && input2 == "no"){
+  if(input3 == "no"){
+   println("Main Street Pizza Company")
+   println("Corner Café")
+   println("Mama’s Fine Italian")
+   println("The Chef’s Kitchen")
+ }
+}else if(input1 == "yes" && input2 == "yes"){
+  if(input3 == "no"){
+  println("Corner Café")   
+  println("The Chef’s Kitchen")
+}
+ }else if(input1 == "yes" && input2 == "no"){
+    if(input3 == "yes"){
+  println("Main Street Pizza Company")  
+  println("Corner Café")   
+  println("The Chef’s Kitchen")  
+}
+  }else if(input1 == "yes" && input2 == "yes"){
+    if(input3 == "yes"){
+  println("Corner Café")   
+  println("The Chef’s Kitchen")
+}
+  }else{
+  println("invalid input")
+  
+}
 }
